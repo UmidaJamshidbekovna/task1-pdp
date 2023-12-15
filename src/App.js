ca792b3291f897abef6572077faa23014eeb7b3b
UmidaJamshidbekovna <78403650+UmidaJamshidbekovna@users.noreply.github.com>
2023-12-16 02:31:04 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import pics from './piccs.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+          <div className="container">
+              <a href="" className='headerText'>Odam Lviran</a>
+              <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+              </ul>
+          </div>
       </header>
+      <section id='homeSection'>
+          <div className="container">
+              <h1>Illustrator</h1>
+              <div className='picsSection'>
+                <img src={pics} alt="" />
+                <img src={pics} alt="" />
+                <img src={pics} alt="" />
+                <img src={pics} alt="" />
+              </div>
+          </div>
+      </section>
+      <section className="footer">
+          <div className="container">
+              <p>© 2035 by Odam Lviran. Powered and secured by </p>
+              <div className="btn">
+                  <button>Facebook</button>
+                  <button>Pinterest </button>
+                  <button>Instagram</button>
+              </div>
+          </div>
+      </section>
     </div>
   );
 }
